f189765a60514eaa422e6c1da6c633fc5e9ef02f
chronosacaria <chronos.droid@gmail.com>
2023-01-02 19:43:16 -0500

    Init Commit  - Set up an example mod

---
+++ src/main/java/timefall/examplemod/Examplemod.java
@@ -0,0 +1,11 @@
+package timefall.examplemod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Examplemod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
